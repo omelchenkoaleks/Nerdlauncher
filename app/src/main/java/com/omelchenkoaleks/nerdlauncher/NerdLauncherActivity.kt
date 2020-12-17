3c56092477fa508856e771c532a863cce00f82e2
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-17 14:41:21 +0200

    Сортируем по алфавиту ResolveInfo и выводим label в списке RecyclerView.

--- app/src/main/java/com/omelchenkoaleks/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/nerdlauncher/NerdLauncherActivity.kt
@@ -1,8 +1,13 @@
 package com.omelchenkoaleks.nerdlauncher
 
 import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -35,6 +40,54 @@ class NerdLauncherActivity : AppCompatActivity() {
          */
         val activities = packageManager.queryIntentActivities(startupIntent, 0)
 
+        /**
+         * Сортировка по алафвиту.
+         */
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+
         Log.i(TAG, "Found ${activities.size} activities")
+
+        recyclerView.adapter = ActivityAdapter(activities)
     }
+
+    private class ActivityHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        private val nameTextView = itemView as TextView
+        private lateinit var resolveInfo: ResolveInfo
+
+        fun bindActivity(resolveInfo: ResolveInfo) {
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+        }
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) :
+        RecyclerView.Adapter<ActivityHolder>() {
+
+        override fun onCreateViewHolder(container: ViewGroup, viewType: Int):
+                ActivityHolder {
+            val layoutInflater = LayoutInflater.from(container.context)
+            val view = layoutInflater
+                .inflate(android.R.layout.simple_list_item_1, container, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return activities.size
+        }
+    }
+
 }
